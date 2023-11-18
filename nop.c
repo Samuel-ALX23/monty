89b5976cb30bb00a057c45d7e8c645d7592f92b3
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-11-18 12:54:44 +0000

    nothing file

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @ignored_stack: unused stack pointer
+ * @ignored_line_num: unused line number
+ *
+ * Return: nothing
+ */
+void nop(stack_t **ignored_stack, unsigned int ignored_line_num)
+{
+	(void) ignored_stack;
+
+	(void) ignored_line_num;
+}
