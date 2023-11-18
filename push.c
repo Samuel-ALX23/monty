6240d7219452f15f173e09a3abb299b0c13ef4f6
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-11-18 19:47:22 +0000

    push

--- push.c
+++ push.c
@@ -1,33 +1,45 @@
 #include "monty.h"
 
 /**
- * push - adds an element to the stack
- * @stack: stack pointer provided by main
- * @lineNumber: line number for error reporting
- *
- * Return: void
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
  */
-void push(stack_t **stack, unsigned int lineNumber)
+
+void f_push(stack_t **head, unsigned int counter)
 {
-	const char *argument = global.argument;
+	int n, j = 0, flag = 0;
 
-	if (!isdigit(*argument)) {
-		fprintf(stderr, "L%u: usage: push integer\n", lineNumber);
-		exit(EXIT_FAILURE);
-	}
-
-	if (global.dataStructure == STACK)
+	if (bus.arg)
 	{
-		if (!add_node(stack, atoi(global.argument)))
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
 		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1;
+		}
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
-		if (!queue_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
