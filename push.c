9c578c7cf48e414e7e8dd3f1b70958b13427ce56
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-11-19 09:28:39 +0000

    push

--- push.c
+++ push.c
@@ -9,27 +9,10 @@
 
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+	int n, j = 0;
+	int flag = 0;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1;
-		}
-		if (flag == 1)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
+	if (bus.arg == NULL || bus.arg[0] == '\0')
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
@@ -37,9 +20,38 @@ void f_push(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
+
+	if (bus.arg[0] == '-')
+	{
+		j++;
+	}
+
+	for (; bus.arg[j] != '\0'; j++)
+	{
+		if (bus.arg[j] < 48 || bus.arg[j] > 57)
+		{
+			flag = 1;
+			break;
+		}
+	}
+
+	if (flag == 1)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
 	n = atoi(bus.arg);
+
 	if (bus.lifi == 0)
+	{
 		addnode(head, n);
+	}
 	else
+	{
 		addqueue(head, n);
+	}
 }
